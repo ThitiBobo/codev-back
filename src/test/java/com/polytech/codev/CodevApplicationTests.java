a18ac72c643bfe5484e4168714b027cf148b544d
Alexandre <alexandre-moine@live.fr>
2021-10-22 10:53:32 +0200

    first commit

---
+++ src/test/java/com/polytech/codev/CodevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.codev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
