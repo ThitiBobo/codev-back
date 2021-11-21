dea927652d0869ad0df0f8ba971102dd93cc4fc7
Alexandre <alexandre-moine@live.fr>
2021-11-21 16:22:34 +0100

    Ajout d'un controller et d'une classe d'appel à une API.

---
+++ src/main/java/com/polytech/codev/appels/AppelApi.java
@@ -0,0 +1,33 @@
+package com.polytech.codev.appels;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class AppelApi {
+
+    public String getData() throws Exception {
+        String data = "";
+        URL url = new URL("https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=eco2mix-metropoles-tr&q=&sort=-code_insee_epci&facet=libelle_metropole&facet=nature&facet=date_heure");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        int responsecode = conn.getResponseCode();
+
+        if(responsecode == 200) {
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNext()) {
+                data += scanner.nextLine();
+            }
+
+            scanner.close();
+        }
+
+        conn.disconnect();
+
+        return data;
+    }
+
+}
---
+++ src/main/java/com/polytech/codev/controllers/DataController.java
@@ -0,0 +1,37 @@
+package com.polytech.codev.controllers;
+
+import com.polytech.codev.appels.AppelApi;
+import org.springframework.web.bind.annotation.*;
+import com.google.gson.Gson;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/data")
+public class DataController {
+
+
+    @GetMapping("/getData")
+    public Object findAllData()
+    {
+        Object data = null;
+
+        try {
+            AppelApi appelApi = new AppelApi();
+            String str = appelApi.getData();
+            Gson gson = new Gson();
+            Type listType = new TypeToken<Object>(){}.getType();
+            data = gson.fromJson(str, listType);
+            //LinkedTreeMap<Object, Object> test = (LinkedTreeMap<Object, Object>) data;
+            //data = test.get("records");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return data;
+    }
+
+
+}
