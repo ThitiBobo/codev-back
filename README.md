3bb433a3df77ac110604d671861dd65b55d17ed4
Thibaut Delplanque <t.delplanque92@gmail.com>
2021-12-13 09:12:54 +0100

    DEV : création du model User + tale

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # codev-back
 
 [electricitymap-contrib/DATA_SOURCE](https://github.com/tmrowco/electricitymap-contrib/blob/master/DATA_SOURCES.md)
+
+
--- src/main/java/com/polytech/codev/appels/AppelApi.java
+++ src/main/java/com/polytech/codev/appels/AppelApi.java
@@ -8,7 +8,7 @@ public class AppelApi {
 
     public String getData() throws Exception {
         String data = "";
-        URL url = new URL("https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=eco2mix-metropoles-tr&q=&sort=-code_insee_epci&facet=libelle_metropole&facet=nature&facet=date_heure");
+        URL url = new URL("https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=eco2mix-metropoles-tr&q=&sort=-code_insee_epci&facet=libelle_metropole&facet=nature&facet=date_heure&facet=date");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
